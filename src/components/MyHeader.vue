2af1c675d8af509b3b0ca0b82a377396127cfdfb
Linuxpert <g.manfre10@gmail.com>
2021-12-13 12:24:29 +0100

    added logo and menu icon

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="containerHeader">
+    <div class="logoMenu">
+        <img src="../../src/assets/avada-music-logo.png" alt="">
+
+        <i class="fas fa-bars"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+// import MyHeader from '@/components/MyHeader.vue'
+
+export default {
+  name: 'MyHeader',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+
+#containerHeader {
+  background-image: url(../../src/assets/home_slider.jpg);
+  height: 200px;
+  .logoMenu{
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      img {
+          margin: 20px;
+      }
+      .fa-bars{
+          margin: 20px;
+          font-size: 40px;
+      }
+  }
+}
+</style>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'MyMain',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
